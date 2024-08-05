924af9c7992964fae30fcea27342924eb9ac9d1b
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 12:20:18 -0400

    Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>hi</h1>}></Route>
+        <Route path="/new" element={<h1>new</h1>}></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>}></Route>
+          <Route path="edit" element={<h1>Edit</h1>}></Route>
+        </Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Routes>
+    </Container>
+  );
 };
 
 export default App;
