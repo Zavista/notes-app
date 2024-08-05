cd86d863ffdc7e9b78691ce2f223d27e5fed0d6a
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 13:00:02 -0400

    Changed element to NewNotePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import NewNotePage from "./pages/NewNotePage";
 
 const App = () => {
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>hi</h1>}></Route>
-        <Route path="/new" element={<h1>new</h1>}></Route>
+        <Route path="/new" element={<NewNotePage></NewNotePage>}></Route>
         <Route path="/:id">
           <Route index element={<h1>Show</h1>}></Route>
           <Route path="edit" element={<h1>Edit</h1>}></Route>
