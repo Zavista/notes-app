922ae772bdc2c32474552411a2bfca3f08769a93
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 13:05:12 -0400

    Initial Form setup

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,5 +1,26 @@
+import { Form, Col, Row, Stack } from "react-bootstrap";
+
 const NoteForm = () => {
-  return <div>NoteForm</div>;
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Select required></Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Stack>
+    </Form>
+  );
 };
 
 export default NoteForm;
