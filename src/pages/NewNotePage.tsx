a3c8bbe2593ef68fea4d3cae6fbf113e9041e5cb
Zavista <84294353+Zavista@users.noreply.github.com>
2024-08-05 13:00:10 -0400

    NoteForm setup

---
+++ src/pages/NewNotePage.tsx
@@ -0,0 +1,12 @@
+import NoteForm from "../components/NoteForm.tsx";
+
+const NewNotePage = () => {
+  return (
+    <h1 className="mb-4">
+      <div className="">New Note</div>
+      <NoteForm></NoteForm>
+    </h1>
+  );
+};
+
+export default NewNotePage;
